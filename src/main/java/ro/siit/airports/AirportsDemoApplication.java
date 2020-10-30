058671201f300c402471bf55f940e07d3fc68c00
Danut Chindris <danutchindris@gmail.com>
2020-10-30 18:46:14 +0200

    create Spring Boot project structure

---
+++ src/main/java/ro/siit/airports/AirportsDemoApplication.java
@@ -0,0 +1,12 @@
+package ro.siit.airports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportsDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/ro/siit/airports/AirportsDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package ro.siit.airports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
